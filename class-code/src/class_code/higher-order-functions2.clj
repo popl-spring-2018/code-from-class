2639b812d9aa644d2b464ec6b80a3c3afadbcabe
Tom Helmuth <thelmuth@hamilton.edu>
2018-02-28 13:43:35 -0500

    Code from 2/28/18. Higher-order functions, closures, and recursion

---
+++ class-code/src/class_code/higher-order-functions2.clj
@@ -0,0 +1,93 @@
+(ns clojure-for-class.higher-order-functions2)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Practice as a class:
+;; Function that finds all prime numbers less than input upper-bound
+
+(defn prime?
+  "Returns true if x is prime, false otherwise."
+  [x]
+  (if (< x 2)
+    false
+    (empty?
+     (filter (fn [divisor] (zero? (mod x divisor)))
+             (range 2 x)))))
+
+(defn primes
+  "Returns list of all prime numbers less than upper-bound."
+  [upper-bound]
+  (filter prime? (range upper-bound)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; --Higher-order function-- that returns a function
+(defn square-old [x] (* x x))
+(defn cube-old [x] (* x x x))
+
+(defn powerer
+  "Returns a function that raises argument to exponent power"
+  [exponent]
+  (fn [argument] ; NOTE: returns an anonymous function
+    (apply * (repeat exponent argument))))
+
+(def cube (powerer 3))
+
+(cube 5)
+
+(def to-the-tenth (powerer 10))
+
+(to-the-tenth 2)
+(to-the-tenth 3.7)
+
+((powerer 10) 2)
+
+; This is a _closure_ - a function with an environment,
+; which gives each free variable a value at the time
+; the function was created
+; The free variable in this case is exponent
+
+
+(defn concat-string-with
+  "Returns a function that takes a string and concats it
+  onto start-string"
+  [start-string]
+  (fn [s]
+    (str start-string s)))
+
+(def say-hello (concat-string-with "hello "))
+
+(say-hello "Susan")
+(say-hello "Bill")
+
+(def t-minus (concat-string-with "T-minus "))
+
+(t-minus 10)
+(map t-minus (reverse (range 10)))
+
+;; Another closure example
+
+(defn log-fn-then-execute
+  "Logs (prints) the function and then executes it.
+  Note: this function takes a function as an argument, and also
+  returns a function."
+  [f]
+  (fn [& args]
+    (println (conj args f))
+    (apply f args)))
+
+(def logged+ (log-fn-then-execute +))
+(logged+ 4 3)
+(logged+ 1 2 3 4 5)
+
+(def logged-str (log-fn-then-execute str))
+(logged-str "hi" "there")
+(map logged-str (range 5))
+
+(defn square
+  [x]
+  (* x x))
+
+(def logged-square (log-fn-then-execute square))
+(logged-square 5)
+
+(def logged-anon-square (log-fn-then-execute #(* % %)))
+(logged-anon-square 3)
---
+++ class-code/src/class_code/recursion.clj
@@ -0,0 +1,103 @@
+(ns clojure-for-class.recursion)
+
+(def nums (range 4 12))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+; --Recursion--
+(defn my-count
+  [lst]
+  (if (empty? lst)
+    0
+    (inc (my-count (rest lst)))))
+
+(my-count nums)
+
+(my-count (range 100000))
+;; Error, stack overflow
+;; Recursive calls in Clojure consume stack space, and Java
+;; isn't very friendly to this recursion. More later.
+
+(defn fib
+  "The nth Fibonacci number"
+  [n]
+  (cond
+    (= n 0) 0
+    (= n 1) 1
+    :else  (+ (fib (dec n))
+              (fib (dec (dec n))))))
+
+(fib 5)
+
+(map fib (range 10))
+
+(fib 100) ; Don't do -- will take a long time!
+
+(defn my-reduce
+  ([function coll]
+    (my-reduce function (first coll) (rest coll)))
+  ([function start-val coll]
+    (if (empty? coll)
+      start-val
+      (my-reduce function
+                 (function start-val
+                           (first coll))
+                 (rest coll)))))
+
+(my-reduce + nums)
+
+(my-reduce list '() nums)
+
+
+; Tail recursion
+; By using recur instead of a recursive call, and not doing anything
+; besides returning the recursive result, Clojure doesn't have to track
+; the recursive calls and can be more efficient
+
+; With recur, can't do anything with the result of the recursive call
+
+(defn first-positive-in-list
+  [numbers]
+  (cond
+    (empty? numbers) nil
+    (< 0 (first numbers)) (first numbers)
+    :else (recur (rest numbers))))
+
+(first-positive-in-list '(-3 -2 -6 4 -2 77))
+
+; This would stack overflow without recur
+(first-positive-in-list (range -1000000 5))
+
+; loop simply sets the recursion point for recur, and declares
+; parameters with their initial values.
+; Note: it is not a loop, it is recursion.
+
+(defn my-tail-count
+  [in-lst]
+  (loop [lst in-lst
+         c 0]
+    (if (empty? lst)
+      c
+      (recur (rest lst)
+             (inc c)))))
+
+(my-tail-count '(1 2 3 4 5 6 7))
+
+(my-tail-count (range 100000))
+
+
+(defn my-tail-fib
+  [n]
+  (loop [n n
+         x 0
+         y 1]
+    (if (<= n 0)
+      x
+      (recur (dec n)
+             y
+             (+' x y)))))
+
+(map my-tail-fib (range 10))
+
+(my-tail-fib 100)
+
+(my-tail-fib 100000)
