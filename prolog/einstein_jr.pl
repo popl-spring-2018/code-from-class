f535336ceb4856c4eff444c2116258fc07691cf6
Tom Helmuth <thelmuth@hamilton.edu>
2018-04-25 11:59:12 -0400

    Added code for next class.

---
+++ prolog/einstein_jr.pl
@@ -0,0 +1,6 @@
+
+% Facts:
+% 1. Two people live in 2 houses with 2 colors and 2 pets.
+% 2. The person in the red house has a giraffe
+% 3. The first house is blue
+% 4. One person owns an elephant as a pet
